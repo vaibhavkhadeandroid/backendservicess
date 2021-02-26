b92eaa6ad0ed9829968c19e4815b183f42478615
vaibhav <vaibhav@DESKTOP-NNGV9L8>
2021-02-26 09:35:59 -0800

    added s3 bucket to and got images url

--- demo/src/main/java/com/ecommerce/vaibhav/DemoApplication.java
+++ demo/src/main/java/com/ecommerce/vaibhav/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.ecommerce.vaibhav;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -10,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+			SpringApplication.run(DemoApplication.class, args);
 	System.out.print("working...");
 	}
 
--- demo/src/main/java/com/ecommerce/vaibhav/repository/UserRepository.java
+++ demo/src/main/java/com/ecommerce/vaibhav/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package com.ecommerce.vaibhav.repository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
 
 import com.ecommerce.vaibhav.model.UserDao;
 public interface UserRepository extends CrudRepository<UserDao, Integer> {
